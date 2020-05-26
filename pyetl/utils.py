148853417d707c0e7f4ae77419a4a75352318975
liyatao <li_yatao@outlook.com>
2020-05-26 15:39:39 +0800

    添加单例元类实现

--- pyetl/utils.py
+++ pyetl/utils.py
@@ -7,6 +7,18 @@ import time
 import functools
 
 
+class Singleton(type):
+
+    def __init__(cls, *args, **kwargs):
+        cls.__instance = None
+        super().__init__(*args, **kwargs)
+
+    def __call__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__call__(*args, **kwargs)
+        return cls.__instance
+
+
 def lower_columns(x):
     if isinstance(x, (list, tuple)):
         return tuple([i.lower() for i in x])
