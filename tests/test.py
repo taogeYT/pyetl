b7089f185a9410991b858dbfd27c79a364da93e9
liyatao <li_yatao@outlook.com>
2020-05-28 13:16:07 +0800

    添加Reader和Writer单元测试

--- tests/test.py
+++ tests/test.py
@@ -3,10 +3,65 @@
 @time: 2020/5/6 4:30 下午
 @desc:
 """
-from pyetl import Task, Reader, Writer, Mapping
+import os
+import unittest
 
-r = Reader("mysql+pymysql://root:lyt124878@localhost:3306/test", "src")
-w = Writer("mysql+pymysql://root:lyt124878@localhost:3306/test", "dst")
-m1 = Mapping(columns={"uuid": "a", "name": "b"}, functions={"uuid": lambda x: x + 20})
-task = Task(r, m1, w)
-task.start()
+from pydbclib import connect
+
+from pyetl.dataset import Dataset
+from pyetl.jobs import Task
+from pyetl.reader import DatabaseReader, FileReader, ExcelReader
+from pyetl.writer import DatabaseWriter, FileWriter
+
+
+class BaseTest(unittest.TestCase):
+    db = None
+    src_record = {"uuid": 1, "full_name": "python ETL framework"}
+    dst_record = {"id": 1, "name": "python ETL framework"}
+    columns = {"uuid": "id", "full_name": "name"}
+
+    @classmethod
+    def get_file_path(cls, name):
+        return os.path.join(os.path.dirname(__file__), 'data', name)
+
+    @classmethod
+    def setUpClass(cls):
+        path = cls.get_file_path("src.db")
+        path = "sqlite:///" + path
+        cls.db = connect(path)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.db.close()
+
+
+class TestReader(BaseTest):
+
+    def validate(self, reader):
+        self.assertEqual(reader.columns, ["uuid", "full_name"])
+        r = reader.read(columns=self.columns)
+        self.assertTrue(isinstance(r, Dataset))
+        self.assertEqual(r.get_all(), [self.dst_record])
+
+    def test_db_reader(self):
+        reader = DatabaseReader(self.db, "src")
+        self.validate(reader)
+
+    def test_file_reader(self):
+        reader = FileReader(self.get_file_path("src.txt"))
+        self.validate(reader)
+
+    def test_excel_reader(self):
+        reader = ExcelReader(self.get_file_path("src.xlsx"))
+        self.validate(reader)
+
+
+class TestWriter(BaseTest):
+
+    def test_db_reader(self):
+        writer = DatabaseWriter(self.db, "dst")
+        writer.table.delete("1=1")
+        ds = Dataset(iter([self.dst_record]))
+        ds.write(writer)
+        r = Task(DatabaseReader(self.db, "dst")).read_and_mapping()
+        self.assertEqual(r.get_all(), [self.dst_record])
