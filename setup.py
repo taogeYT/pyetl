32dcb9c1227af9e829863da48dd53b8f0c47f156
liyatao <li_yatao@outlook.com>
2020-06-04 22:55:46 +0800

    添加es依赖

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('pyetl/__init__.py', 'rb') as f:
 setup(
     name='pyetl',
     version=version,
-    install_requires=['pydbclib>=2.1.8', 'pandas>=0.22'],
+    install_requires=['pydbclib>=2.1.8', 'pandas>=0.22', 'elasticsearch'],
     description='Python ETL Frame',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
